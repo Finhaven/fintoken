4f98a83277d11ef83c3ddda29c67b6c28d0aa1a1
ginetrix <voskobojnikov.artemij@gmail.com>
2021-07-30 16:29:58 -0700

    added pausable, lockable functionality

--- contracts/Finhaven.sol
+++ contracts/Finhaven.sol
@@ -1,8 +1,12 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import './extensions/ERC20Burnable.sol';
+import './extensions/ERC20Lockable.sol';
+import './extensions/ERC20Pausable.sol';
 
-contract Finhaven is ERC20Burnable{
+contract Finhaven is ERC20Burnable, ERC20Pausable, ERC20Lockable{
 	/**
      * Minting FIN Token with a specified initial supply of 1 billion
      * Owner is the deployer address
@@ -15,4 +19,12 @@ contract Finhaven is ERC20Burnable{
     ) ERC20(name, symbol) {
         _mint(owner, initialSupply);
     }
+
+    function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual override(ERC20, ERC20Pausable, ERC20Lockable) {
+    super._beforeTokenTransfer(from, to, amount);
+  }
 }
